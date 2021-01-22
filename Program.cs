46dd84a8b62d442d80ed6416d2c65d8dbd69088b
Gregoriy Milushev <burzakata@gmail.com>
2021-01-22 12:07:30 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionaryOfPokemonTrainers = new Dictionary<string, Trainer>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "Tournament")
+                {
+                    break;
+                }
+                var info = input.Split();
+                var trainerName = info[0];
+                var pokemonName = info[1];
+                var pokemonElement = info[2];
+                var pokemonHealth = int.Parse(info[3]);
+
+                var pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+                var trainer = new Trainer(trainerName, pokemon);
+
+                if (!dictionaryOfPokemonTrainers.ContainsKey(trainerName))
+                {
+                    dictionaryOfPokemonTrainers.Add(trainerName, trainer);
+                }
+                else
+                {
+                    dictionaryOfPokemonTrainers[trainerName].Pokemons.Add(pokemon);
+                }
+            }
+
+            while (true)
+            {
+                var element = Console.ReadLine();
+                if (element == "End")
+                {
+                    break;
+                }
+
+                foreach (var trainer in dictionaryOfPokemonTrainers)
+                {
+                    trainer.Value.ContainsPokemon(element);
+                }
+            }
+
+            foreach (var (name,trainer) in dictionaryOfPokemonTrainers
+                .OrderByDescending(x => x.Value.Badgets))
+            {
+                Console.WriteLine($"{name} {trainer.Badgets} {trainer.Pokemons.Count}");Q
+            }
+        }
+    }
+}
---
+++ Trainer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        private string name;
+        private int badgets = 0;
+        private List<Pokemon> pokemons = new List<Pokemon>();
+
+        public string Name { get => name; set => name = value; }
+
+        public int Badgets { get => badgets; set => badgets = value; }
+
+        public List<Pokemon> Pokemons { get => pokemons; set => pokemons = value; }
+
+        public Trainer(string name, Pokemon pokemon)
+        {
+            this.Name = name;
+            this.Pokemons.Add(pokemon);
+        }
+
+        public void ContainsPokemon(string element)
+        {
+            bool isContained = false;
+            foreach (var pokemon in Pokemons)
+            {
+                if (pokemon.Element == element)
+                {
+                    isContained = true;
+                    this.Badgets += 1;
+                    break;
+                }
+            }
+
+            if (isContained == false)
+            {
+                var pokemonsDeath = new List<Pokemon>();
+                foreach (var pokemon in Pokemons)
+                {
+                    pokemon.Health -= 10;
+                    if (pokemon.Health <= 0)
+                    {
+                        pokemonsDeath.Add(pokemon);
+                    }
+                }
+
+                foreach (var pokemon in pokemonsDeath)
+                {
+                    Pokemons.Remove(pokemon);
+                }
+            }
+        }
+    }
+}
